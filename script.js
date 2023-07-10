f1ed6c9f325628c5e0244233dc4767a85a42d5f5
richard tsang <richard.tsang9641@gmail.com>
2023-07-10 13:14:33 +0800

    add control to color chaging background

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const dragAndSpin = (function () {
   const target = document.querySelector(".ticket-wrapper");
+  const targetColor = document.querySelector(".color-bg");
   let timeStart, touchPosStart, touchDegLast, isMoved;
   let deg = +(target.style.transform.match(/\(([^)]+)deg/)?.[1] || 0);
   //touch event
@@ -9,8 +10,12 @@ const dragAndSpin = (function () {
   });
   target.addEventListener("touchend", (e) => {
     target.classList.add("smooth");
+    targetColor.classList.add("smooth");
+    let prevDeg = deg;
     if (touchPosStart > 0) {
+      //left click
       if (isMoved) {
+        //from left to right
         if (touchDegLast > 90) {
           deg += 180;
         }
@@ -18,7 +23,9 @@ const dragAndSpin = (function () {
         deg -= 180;
       }
     } else {
+      //right click
       if (isMoved) {
+        //from right to left
         if (touchDegLast < -90) {
           deg -= 180;
         }
@@ -26,7 +33,50 @@ const dragAndSpin = (function () {
         deg += 180;
       }
     }
+
     target.style.transform = `rotateY(${deg}deg)`;
+    //color background position changing
+    if (deg % 360 === 0) {
+      //go to front side
+      if (isMoved) {
+        targetColor.style.backgroundPosition = `50% 50%`;
+      } else {
+        if (touchPosStart < 0) {
+          //back right
+          console.log(
+            "case 1 - back right click",
+            targetColor.style.backgroundPosition
+          );
+          if (targetColor.style.backgroundPosition === "150% 50%") {
+            targetColor.style.backgroundPosition = `-50% 50%`;
+          }
+          targetColor.style.backgroundPosition = `50% 50%`;
+        } else {
+          console.log(
+            "case 2 - back left click",
+            targetColor.style.backgroundPosition
+          );
+          if (targetColor.style.backgroundPosition === "-50% 50%") {
+            targetColor.style.backgroundPosition = `150% 50%`;
+          }
+          targetColor.style.backgroundPosition = `50% 50%`;
+        }
+      }
+    } else {
+      //go to  back side
+      if (isMoved) {
+        targetColor.style.backgroundPosition = `-50% 50%`;
+      } else {
+        if (touchPosStart < 0) {
+          console.log("case 3 - front right click", (deg - prevDeg) % 360);
+          targetColor.style.backgroundPosition = `150% 50%`;
+        } else {
+          console.log("case 4 - front left click", (deg - prevDeg) % 360);
+          targetColor.style.backgroundPosition = `-50% 50%`;
+        }
+      }
+    }
+
     isMoved = false;
   });
 
@@ -37,6 +87,9 @@ const dragAndSpin = (function () {
     }
     if (target.classList.contains("smooth")) {
       target.classList.remove("smooth");
+    }
+    if (targetColor.classList.contains("smooth")) {
+      targetColor.classList.remove("smooth");
     }
 
     target.style.transform = `rotateY(${
@@ -49,5 +102,38 @@ const dragAndSpin = (function () {
       ((touchPosStart - (screen.width / 2 - e.touches[0].clientX)) /
         target.clientWidth) *
       180;
+
+    console.log(deg);
+    // console.log(((touchPosStart - (screen.width / 2 - e.touches[0].clientX)) /
+    // target.clientWidth) *
+    // 100);
+
+    //color background position changing
+    if (deg % 360 === 0) {
+      targetColor.style.backgroundPosition = `${
+        ((touchPosStart - (screen.width / 2 - e.touches[0].clientX)) /
+          target.clientWidth) *
+          100 +
+        50
+      }% 50%`;
+    } else {
+      if (touchPosStart < 0) {
+        targetColor.style.backgroundPosition = `${
+          ((touchPosStart - (screen.width / 2 - e.touches[0].clientX)) /
+            target.clientWidth) *
+            100 +
+          150
+        }% 50%`;
+      } else {
+        targetColor.style.backgroundPosition = `${
+          ((touchPosStart - (screen.width / 2 - e.touches[0].clientX)) /
+            target.clientWidth) *
+            100 -
+          50
+        }% 50%`;
+      }
+    }
   });
 })();
+
+//+150 or -50
